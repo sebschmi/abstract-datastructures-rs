29b88c4dd41c810f72a83a8ccd31ca3065e0c9b9
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-09-20 11:42:16 +0300

    Add more methods to editable sequence.

--- traitsequence/src/implementation/vec.rs
+++ traitsequence/src/implementation/vec.rs
@@ -24,7 +24,38 @@ impl<Item> OwnedSequence<Item, [Item]> for Vec<Item> {}
 impl<Item: Clone> CloneableSequence<Item, [Item]> for Vec<Item> {}
 
 impl<Item> EditableSequence<Item, [Item]> for Vec<Item> {
+    fn set(&mut self, index: usize, item: Item) {
+        self[index] = item;
+    }
+
     fn split_off(&mut self, at: usize) -> Self {
         Vec::split_off(self, at)
+    }
+
+    fn reserve(&mut self, additional: usize) {
+        self.reserve(additional);
+    }
+
+    fn resize(&mut self, new_len: usize, value: Item)
+    where
+        Item: Clone,
+    {
+        self.resize(new_len, value);
+    }
+
+    fn resize_with(&mut self, new_len: usize, generator: impl FnMut() -> Item) {
+        self.resize_with(new_len, generator);
+    }
+
+    fn push(&mut self, item: Item) {
+        self.push(item)
+    }
+
+    fn splice(
+        &mut self,
+        range: std::ops::Range<usize>,
+        replace_with: impl IntoIterator<Item = Item>,
+    ) {
+        self.splice(range, replace_with);
     }
 }
